ee296c74a72d953c777f1255e83497a274f0923f
PedroDiassss <pedrohenriquedesouzadias640@gmail.com>
2026-05-15 23:25:12 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -34,3 +34,11 @@ export default defineConfig({
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/manimve-culos/',
+})
